df25883d00f9f79355ee5504a3ac9bae28c29ca5
djr-taureau <djr.taureau@gmail.com>
2020-03-25 13:16:59 -0500

    ngrx config

--- apps/hza/src/app/app.module.ts
+++ apps/hza/src/app/app.module.ts
@@ -4,10 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ClarityModule, BrowserAnimationsModule],
+  imports: [BrowserModule, ClarityModule, BrowserAnimationsModule, StoreModule.forRoot(reducers, {
+      metaReducers
+    }), StoreRouterConnectingModule.forRoot(), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })
