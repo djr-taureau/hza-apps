3cdafebcb20b41fd9618591512ba34813fabc70c
djr-taureau <djr.taureau@gmail.com>
2020-03-26 06:01:12 -0500

    completed ui comps and packages

--- apps/hza/src/app/app.module.ts
+++ apps/hza/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CoreModule } from '@hza/core';
 import { ClarityModule } from '@clr/angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
 
+const routes: Routes = [];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, ClarityModule, BrowserAnimationsModule],
+  imports: [BrowserModule, CoreModule, ClarityModule, BrowserAnimationsModule, RouterModule.forRoot(routes), ReactiveFormsModule, FormlyModule.forRoot(), FormlyBootstrapModule,],
   providers: [],
   bootstrap: [AppComponent]
 })
