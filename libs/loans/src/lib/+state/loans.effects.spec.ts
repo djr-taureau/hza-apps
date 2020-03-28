cb6d4dbb026f672bcd235700a81893d7b6c81c68
djr-taureau <djr.taureau@gmail.com>
2020-03-28 10:10:08 -0500

    jest unit tests

--- libs/loans/src/lib/+state/loans.effects.spec.ts
+++ libs/loans/src/lib/+state/loans.effects.spec.ts
@@ -8,46 +8,60 @@ import { Observable } from 'rxjs';
 import * as LoanActions from './loans.actions';
 import { LoanEffects } from './loans.effects';
 import { Loan } from './../models/loan.model';
-import { LoansFacade } from './loans.facade';
+import { LoansService } from '../services/loans.service';
 
-const loan1 = { id: 12 } as Loan;
-const loan2 = { id: 14 } as Loan;
+const loan1 = {
+	id: 12,
+	LoanNumber: '0000123456',
+	SourceID: 1,
+	Name: 'Mary Smith',
+	Phone: '2142434444',
+	City: 'Dallas'
+} as Loan;
+const loan2 = {
+	id: 14,
+	LoanNumber: '0000454111',
+	SourceID: 1,
+	Name: 'Mary Jones',
+	Phone: '2122434444',
+	City: 'New York'
+} as Loan;
 
 describe('LoanEffects', () => {
-  let effects: LoanEffects;
-  let loansFacade: any;
-  let actions$: Observable<any>;
+	let effects: LoanEffects;
+	let loansService: any;
+	let actions$: Observable<any>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        LoanEffects,
-        {
-          provide: LoansFacade,
-          useValue: { loadLoans: jest.fn() },
-        },
-        provideMockActions(() => actions$),
-      ],
-    });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [
+				LoanEffects,
+				{
+					provide: LoansService,
+					useValue: { loadLoans: jest.fn() }
+				},
+				provideMockActions(() => actions$)
+			]
+		});
 
-    effects = TestBed.inject(LoanEffects);
-    loansFacade = TestBed.inject(LoansFacade);
-    actions$ = TestBed.inject(Actions);
-  });
+		effects = TestBed.inject(LoanEffects);
+		loansService = TestBed.inject(LoansService);
+		actions$ = TestBed.inject(Actions);
+	});
 
-  describe('loadLoans$', () => {
-    it('should return a loans.LoadSuccess, with the loans, on success', () => {
-      const action = LoanActions.loadLoans();
-      const completion = LoanActions.loadLoansSuccess({
-        loans: [loan1, loan2],
-      });
+	describe('loadLoans$', () => {
+		it('should return a loans.LoadSuccess, with the loans, on success', () => {
+			const action = LoanActions.loadLoans();
+			const completion = LoanActions.loadLoansSuccess({
+				loans: [loan1, loan2]
+			});
 
-      actions$ = hot('-a', { a: action });
-      const response = cold('-a|', { a: [loan1, loan2] });
-      const expected = cold('--c', { c: completion });
-      loansFacade.loadLoans = jest.fn(() => response);
+			actions$ = hot('-a', { a: action });
+			const response = cold('-a|', { a: [loan1, loan2] });
+			const expected = cold('--c', { c: completion });
+			loansService.getLoans = jest.fn(() => response);
 
-      expect(effects.loadLoans$).toBeObservable(expected);
-    });
-  });
+			expect(effects.loadLoans$).toBeObservable(expected);
+		});
+	});
 });
