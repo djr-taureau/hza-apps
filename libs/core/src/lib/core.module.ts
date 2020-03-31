6e0a8aa6c549cd42fde248f562219eca70777b5d
djr-taureau <djr.taureau@gmail.com>
2020-03-31 17:31:54 -0500

    http interceptors and core services

--- libs/core/src/lib/core.module.ts
+++ libs/core/src/lib/core.module.ts
@@ -3,16 +3,24 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedUtilsModule } from '@hza/shared/utils';
 import { SharedDataAccessModule } from '@hza/shared/data-access';
-
-import { ApiEndpointService } from './services/api-endpoint.service';
-import { ApiService } from './services/api.service';
+import { httpInterceptorProviders } from './http-interceptors';
+import { ApiService, ApiEndpointService, EventBusService, NotificationService, CacheMapService } from './services';
 import { LazyLoaderService } from './lazy-components/lazy-loader.service';
 import { LAZY_WIDGETS } from './lazy-components/tokens';
 import { lazyArrayToObj } from './lazy-components/lazy-widgets';
 
 @NgModule({
 	imports: [CommonModule, SharedUtilsModule, SharedDataAccessModule.forRoot()],
-	providers: [ApiEndpointService, ApiService, LazyLoaderService, { provide: LAZY_WIDGETS, useFactory: lazyArrayToObj }]
+	providers: [
+		ApiEndpointService,
+		ApiService,
+		EventBusService,
+		NotificationService,
+		CacheMapService,
+		LazyLoaderService,
+		httpInterceptorProviders,
+		{ provide: LAZY_WIDGETS, useFactory: lazyArrayToObj }
+	]
 })
 export class CoreModule {
 	constructor(
