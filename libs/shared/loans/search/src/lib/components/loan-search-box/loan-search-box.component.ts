a864b0f46d754cd0f95e610770edbdcb48d53b8d
djr-taureau <djr.taureau@gmail.com>
2020-04-18 16:42:41 -0500

    styling for search box

--- libs/shared/loans/search/src/lib/components/loan-search-box/loan-search-box.component.ts
+++ libs/shared/loans/search/src/lib/components/loan-search-box/loan-search-box.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'hza-loan-search-box',
   templateUrl: './loan-search-box.component.html',
-  styleUrls: ['./loan-search-box.component.css']
+  styleUrls: ['./loan-search-box.component.scss']
 })
 export class LoanSearchBoxComponent implements OnInit {
 
