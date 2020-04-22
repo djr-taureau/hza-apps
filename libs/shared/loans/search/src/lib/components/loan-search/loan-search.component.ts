e10ec8210ace232ee89ba289311cb98b3a1493fe
djr-taureau <djr.taureau@gmail.com>
2020-04-22 18:14:56 -0500

    search help messages

--- libs/shared/loans/search/src/lib/components/loan-search/loan-search.component.ts
+++ libs/shared/loans/search/src/lib/components/loan-search/loan-search.component.ts
@@ -1,8 +1,12 @@
 import { ComponentType } from '@angular/cdk/portal';
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, TemplateRef } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { faTimesCircle } from '@fortawesome/free-regular-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Loan } from '@hza/shared/loans/models';
 import { OverlayService, PopoverService } from '@hza/ui-components/overlay';
+import { BehaviorSubject } from 'rxjs';
+import { helpMessages } from './loanSearchTypes';
 
 @Component({
 	selector: 'hza-loan-search',
@@ -13,6 +17,10 @@ export class LoanSearchComponent implements OnInit, OnChanges {
 	loanSearchComponent = LoanSearchComponent;
 	loanSearchComponentResponse = null;
 	form: FormGroup;
+	faTimes = faTimes;
+	faTimesCircle = faTimesCircle;
+	searchHelpMessage: string;
+	searchTypeSubject$ = new BehaviorSubject<String>('loan');
 
 	loanSearchBox = new FormControl('');
 	@Input() loansLoaded: boolean;
@@ -31,11 +39,39 @@ export class LoanSearchComponent implements OnInit, OnChanges {
 			loanSearch: ''
 		});
 		this.form.valueChanges.subscribe(console.log);
+		// let idx = this.searchType.getValue();
+
+		this.form.get('loan').valueChanges.subscribe((v) => {
+			console.log('init', v);
+			this.searchTypeSubject$.next(v);
+			const searchTypeIdx = this.searchTypeSubject$.value;
+			console.log('give me the type', searchTypeIdx)
+			this.searchHelpMessage = helpMessages[`${searchTypeIdx}`].message;
+		});
+		this.searchTypeSubject$.subscribe(v => console.log('give me the tyep sub', v))
 	}
 
 	ngOnChanges() {
-		// console.log(this.form.value)
+
+
+		this.form.get('loan').valueChanges.subscribe((v) => {
+			console.log('changes', v)
+			this.searchTypeSubject$.next(v);
+		});
+		this.searchTypeSubject$.subscribe((v) => {
+			this.searchHelpMessage = helpMessages[`${v}`].message;
+		});
 	}
+
+	// onValueChanges(): void {
+	// 	const helpMessages = indexByProp('value', loanSearchTypes);
+	// 	this.form.get('loan').valueChanges.subscribe((v) => {
+	// 		this.searchHelpMessage = helpMessages[v].message;
+	// 	});
+	// }
+	// ngOnChanges() {
+	// 	// console.log(this.form.value)
+	// }
 
 	dispatch($event) {
 		console.log($event);
@@ -58,7 +94,6 @@ export class LoanSearchComponent implements OnInit, OnChanges {
 			console.log('show', res);
 			this.updateSearchBox();
 			console.log(this.form.get('loanSearch').value);
-			
 		});
 	}
 
@@ -77,12 +112,12 @@ export class LoanSearchComponent implements OnInit, OnChanges {
 		console.log(value);
 	}
 
-	open(content: TemplateRef<any> | ComponentType<any> | string) {
-		const ref = this.overlayService.open(content, null);
+	// open(content: TemplateRef<any> | ComponentType<any> | string) {
+	// 	const ref = this.overlayService.open(content, null);
 
-		ref.afterClosed$.subscribe((res) => {
-			console.log('open', res);
-			console.log(this.form.get('loanSearch').value);
-		});
-	}
+	// 	ref.afterClosed$.subscribe((res) => {
+	// 		console.log('open', res);
+	// 		console.log(this.form.get('loanSearch').value);
+	// 	});
+	// }
 }
