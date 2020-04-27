de6e914af8b35b248f320d8ddc90feb41b21c5d9
djr-taureau <djr.taureau@gmail.com>
2020-04-27 10:10:50 -0500

    field focus directive

---
+++ libs/shared/utils/src/lib/directives/focus/focus.directive.ts
@@ -0,0 +1,16 @@
+import { AfterViewInit, Directive, ElementRef, HostBinding } from '@angular/core';
+
+@Directive({
+	selector: '[hzaFocus]',
+})
+export class FocusDirective implements AfterViewInit {
+	constructor(private host: ElementRef) {}
+
+	// @HostBinding('attr.class')
+	// get hostClass(): string {
+	// 	return 'field-focus';
+	// }
+	ngAfterViewInit() {
+		this.host.nativeElement.focus();
+	}
+}
--- libs/shared/utils/src/lib/shared-utils.module.ts
+++ libs/shared/utils/src/lib/shared-utils.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NiceDatePipe } from './pipes/date';
@@ -5,20 +6,16 @@ import { ReplacePipe, AddressPipe, PhonePipe } from './text';
 import { MaterialModule } from './material.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-import {OverlayPanelModule} from 'primeng/overlaypanel';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
-
-
+import { FocusDirective } from './directives/focus/focus.directive';
 import { CopyableDirective } from './directives/copyable.directive';
 import { TooltipDirective } from './directives/tooltip/tooltip.directive';
 
-const COMPONENTS = [
-	NiceDatePipe, ReplacePipe, AddressPipe, PhonePipe, CopyableDirective, TooltipDirective
-];
+const COMPONENTS = [NiceDatePipe, ReplacePipe, AddressPipe, PhonePipe, CopyableDirective, TooltipDirective, FocusDirective];
 @NgModule({
 	imports: [CommonModule, MaterialModule, FontAwesomeModule, OverlayPanelModule, ButtonsModule],
 	declarations: COMPONENTS,
 	exports: [MaterialModule, COMPONENTS, FontAwesomeModule, OverlayPanelModule, ButtonsModule]
 })
 export class SharedUtilsModule {}
-	
