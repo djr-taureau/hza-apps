4d899cae0c41c88f0db210ee6a1f6a0e754763b7
David J Ruyle <djr.taureau@gmail.com>
2020-05-14 05:29:58 -0500

    Sp3 file upload (#39)
    * reusable file upload component
    
    * pipe for calculating file size
    
    * created cards for file upload items
    
    * file preview positioning and styling
    
    * layout for doc preview
    
    * doc info thumbnails
    
    * doc file info button position and style
    
    * clean up

---
+++ libs/ui-components/attachment/src/lib/ui-components-attachment.module.spec.ts
@@ -0,0 +1,14 @@
+import { async, TestBed } from '@angular/core/testing';
+import { UiComponentsAttachmentModule } from './ui-components-attachment.module';
+
+describe('UiComponentsAttachmentModule', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [UiComponentsAttachmentModule],
+    }).compileComponents();
+  }));
+
+  it('should create', () => {
+    expect(UiComponentsAttachmentModule).toBeDefined();
+  });
+});
