e98cb63c2eda246c8bb3299e7de450805f4bb944
djr-taureau <djr.taureau@gmail.com>
2020-05-15 14:12:39 -0500

    docType actions

--- libs/documents/src/lib/+state/documents/documents.actions.ts
+++ libs/documents/src/lib/+state/documents/documents.actions.ts
@@ -3,15 +3,11 @@ import { Update } from '@ngrx/entity';
 import { Document } from './../../models/document.model';
 import { CodeTable } from '../../models/code-table.model';
 
-
-
 export const loadDocs = createAction('[documents] LOAD_DOCS');
-
 export const loadDoc = createAction('[documents] Load Document', props<{ document: Document }>());
 export const selectDoc = createAction('[documents] Select Document', props<{ ID: number }>());
 
 export const loadDocTypes = createAction('[documents] LOAD_DOCS_TYPES');
 export const loadDocTypesSuccess = createAction('[documents] LOAD_DOCTYPES_SUCCESS', props<{ docTypes: CodeTable[] }>())
-
 export const loadDocsSuccess = createAction('[documents] LOAD_DOCS_SUCCESS', props<{ documents: Document[] }>());
 export const loadDocsFail = createAction('[documents] LOAD_DOCS_FAIL', props<{ error: Error }>());
