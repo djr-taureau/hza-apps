dbc1dd6ffb9ae0da09a197de1c1ddc3d91af285e
djr-taureau <djr.taureau@gmail.com>
2020-05-20 10:45:47 -0500

    custom reusable file upload option #4

--- libs/shared/utils/src/lib/+modules/material.module.ts
+++ libs/shared/utils/src/lib/+modules/material.module.ts
@@ -11,6 +11,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -29,6 +30,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 @NgModule({
 	imports: [
 		CommonModule,
+		MatDialogModule,
 		MatButtonModule,
 		MatSelectModule,
 		MatTabsModule,
@@ -56,6 +58,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 	declarations: [],
 	exports: [
 		CommonModule,
+		MatDialogModule,
 		MatButtonModule,
 		MatMenuModule,
 		MatTabsModule,
