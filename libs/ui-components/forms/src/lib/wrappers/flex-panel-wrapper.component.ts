33ab3e1aa95b82ad5a1859234315cf4647c7e7ce
djr-taureau <djr.taureau@gmail.com>
2020-05-27 17:23:03 -0500

    doc detail layout

--- libs/ui-components/forms/src/lib/wrappers/flex-panel-wrapper.component.ts
+++ libs/ui-components/forms/src/lib/wrappers/flex-panel-wrapper.component.ts
@@ -2,8 +2,8 @@ import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 import { FieldWrapper, FormlyTemplateOptions } from '@ngx-formly/core';
 
 @Component({
-  selector: 'flex-formly-wrapper-panel',
-  template: `
+	selector: 'flex-formly-wrapper-panel',
+	template: `
     <div [fxLayout]="to.fxLayout" [fxLayoutAlign]="to.fxLayoutAlign">
       <div class="form-label" [fxLayout]="to.fxLayoutLabel" [fxFlex]="to.fxFlexLabel">{{ to.label }}</div>
       <div class="form-spacer" [fxFlex]="to.fxFlexSpacer"></div>
@@ -14,16 +14,29 @@ import { FieldWrapper, FormlyTemplateOptions } from '@ngx-formly/core';
   `
 })
 export class FlexPanelWrapperComponent extends FieldWrapper {
-  @ViewChild('fieldComponent', {read: ViewContainerRef}) fieldComponent: ViewContainerRef;
-  	to: FormlyTemplateOptions & { fxLayout: string; fxLayoutAlign: string, fxFlexLabel: string, fxFlexSpacer: string, fxFlexField: string, fxLayoutLabel: string, fxLayoutField: string };
+	@ViewChild('fieldComponent', { read: ViewContainerRef })
+	fieldComponent: ViewContainerRef;
+	to: FormlyTemplateOptions & {
+		fxLayout: string;
+		fxLayoutAlign: string;
+		fxFlexLabel: string;
+		fxFlexSpacer: string;
+		fxFlexField: string;
+		fxLayoutLabel: string;
+		fxLayoutField: string;
+		fxLayoutAlignLabel: string;
+		fxLayoutAlignField: string;
+	};
 
 	ngOnInit() {
 		this.to.fxLayout = this.to.fxLayout || 'row';
-		this.to.fxLayoutAlign = this.to.fxLayoutAlign || 'start stretch';
-    this.to.fxFlexLabel = this.to.fxFlexLabel || '40';
-    this.to.fxFlexSpacer = this.to.fxFlexSpacer || '20';
-    this.to.fxFlexField = this.to.fxFlexField || '40';
-    this.to.fxLayoutLabel = this.to.fxLayoutLabel || 'start';
-    this.to.fxLayoutField = this.to.fxLayoutField || 'end';
+		this.to.fxLayoutAlign = this.to.fxLayoutAlign || 'space-around center';
+		this.to.fxFlexLabel = this.to.fxFlexLabel || '40';
+		this.to.fxFlexSpacer = this.to.fxFlexSpacer || '20';
+		this.to.fxFlexField = this.to.fxFlexField || '40';
+		this.to.fxLayoutLabel = this.to.fxLayoutLabel || 'column';
+		this.to.fxLayoutField = this.to.fxLayoutField || 'column';
+    this.to.fxLayoutAlignLabel = this.to.fxLayoutAlignLabel || 'space-around start';
+		this.to.fxLayoutAlignField = this.to.fxLayoutAlignField || 'space-around end';
 	}
 }
